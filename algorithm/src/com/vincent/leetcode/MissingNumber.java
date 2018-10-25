d91a62c69f0737f12f7555a3ba833cb4a8982a9f
Vincent <vincent9101@qq.com>
2018-10-25 21:09:09 +0800

    update Missing Number

--- algorithm/src/com/vincent/leetcode/MissingNumber.java
+++ algorithm/src/com/vincent/leetcode/MissingNumber.java
@@ -2,31 +2,24 @@ package com.vincent.leetcode;
 
 import java.util.Arrays;
 
-public class MIssingNumber {
+public class MissingNumber {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(missingNumber(new int[] { 9, 6, 4, 2, 3, 5, 7, 0, 1 }));
 	}
+
 	public static int missingNumberVersion2(int[] nums) {
 
-		int sum=(nums.length+1)*nums.length/2;
-		int[] temp = new int[nums.length + 1];
-		Arrays.fill(temp, -1);
-		int index = 0;
+		int sum = (nums.length + 1) * nums.length / 2;
+
 		for (int i = 0; i < nums.length; i++) {
-			temp[nums[i]] = nums[i];
+			sum -= nums[i];
 		}
 
-		for (int i = 0; i < temp.length; i++) {
-			if (temp[i] == -1) {
-				index = i;
-				break;
-			}
-		}
-
-		return index;
+		return sum;
 	}
+
 	public static int missingNumber(int[] nums) {
 
 		int[] temp = new int[nums.length + 1];
